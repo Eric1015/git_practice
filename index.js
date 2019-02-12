c4e1e733725a2b4c44c8046c746b9ececa027e10
love1 <love1dbsb77@hotmail.com>
2019-02-11 19:39:10 -0800

    Added answer

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 function calculate() {
     console.log(1 + 2);
+    var number = document.getElementById("number_input").value;
+    var arr = number.split("");
+    var sum = 0;
+    for (var elem of arr) {
+        sum += parseInt(elem, 10);
+    }
+    console.log(sum);
+}
+
+function answer2() {
+    var number = document.getElementById("number_input").value;
+    var sum = 0;
+    for (var i = 0; i < number.length; i++) {
+        sum += parseInt(number.charAt(i), 10);
+    }
+    console.log(sum);
 }
